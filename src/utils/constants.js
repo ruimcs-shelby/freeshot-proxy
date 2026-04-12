9fd131f874c0a91421301279af35aa3c043baafc
Rui Sousa <ruimcs.shelby@gmail.com>
2026-04-12 16:58:34 +0100

    fix: multiple channel channels issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,10 +5,11 @@ export class Constants{
     static errorChannelTokenNotDefined = "Channel token not defined";
     
     // TODO: Evaluate and implement a retry system for each one of these timeouts in the future
+    // TODO: Config?
     // Intervals
-    static tokenUpdateIntervalInMilliseconds = 300_000;         // = 5 minutes
-    static networkIdleTimeOutInMilliseconds = 30_000;           // = 10 seconds
-    static extraTimeoutForJsProcessingInMilliseconds = 10_000;   // = 2 seconds
+    static tokenUpdateIntervalInMilliseconds = 600_000;         // = 10 minutes
+    static networkIdleTimeOutInMilliseconds = 25_000;           // = 25 seconds
+    static extraTimeoutForJsProcessingInMilliseconds = 10_000;  // = 10 seconds
 
     // Static files
     static freeshotDatabaseFile = "database/freeshot.json";
